e1157897f0ae8000aa8774f491d279c6110c40c7
kareemIt <kareem.itani2@gmail.com>
2021-09-22 23:52:29 -0400

    css changes

--- src/component/MovieForm.js
+++ src/component/MovieForm.js
@@ -1,64 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
 const MovieForm = (prop) => {
-  const [movie, setMovie] = useState("");
-  const [rating, setRating] = useState("");
-  const [duration, setDuration] = useState("");
+  const [movie, setMovie] = useState('');
+  const [rating, setRating] = useState('');
+  const [duration, setDuration] = useState('');
 
-  function movieName (e) {
+  function movieName(e) {
     setMovie(e.target.value);
   }
 
-  function movieRating (e) {
-    if (isNaN(e.target.value)) return;
+  function movieRating(e) {
     setRating(e.target.value);
   }
 
-  function movieDuration (e) {
+  function movieDuration(e) {
     const duration = e.target.value.substring(e.target.value.length);
     const hours = 0;
+    //ask derick
+    if (duration.substring(duration.length - 1) == 'm') {
+      hours = duration / 60;
+    }
+    if (duration.substring(duration.length - 1) == 'h') {
+      hours = duration;
+    }
 
-    if (duration.substring(duration.length-1) == "m") {
-        hours = duration/60;
-    }
-    if (duration.substring(duration.length-1) == "h") {
-       hours = duration;
-    }
-    
     setDuration(hours);
   }
 
-  function Sumbit () {
-    prop.setMovie(prop.movieList.concat({movie, rating, duration}));
-    setMovie("");
-    setRating("");
-    setDuration("");
+  function Sumbit() {
+    if (rating > 100) {
+      //ask derick
+      setRating(100);
+    }
+    if (rating < 0) {
+      setRating(0);
+    }
+    prop.setMovie(prop.movieList.concat({ movie, rating, duration }));
+    setMovie('');
+    setRating('');
+    setDuration('');
   }
 
-  return(
+  return (
     <div className="inner-input-container">
       <div className="inner-input">
         <p>Movie name</p>
-        <input 
-        value={movie}
-        className="movie-Name"
-        onChange={movieName}
-        />
+        <input value={movie} className="movie-Name" onChange={movieName} />
         <p>Movie rating</p>
-        <input 
-        value={rating}
-        className="movie-rating"
-        onChange={movieRating}
+        <input
+          value={rating}
+          type="number"
+          min="0"
+          max="100"
+          className="movie-rating"
+          onChange={movieRating}
         />
         <p>Movie duration</p>
-        <input 
-        value={duration}
-        className="movie-duration"
-        onChange={movieDuration}
+        <input
+          value={duration}
+          className="movie-duration"
+          onChange={movieDuration}
         />
         <button onClick={Sumbit}>Sumbit</button>
-        </div>
-    </div>  
+      </div>
+    </div>
   );
 };
 export default MovieForm;
